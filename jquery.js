a4b1e5ab67486fcbb2c1d77bb5bcd6925670f82a
Thomas Lo <thomasjinlo@gmail.com>
2016-03-08 11:15:42 -0800

    Build your own jQuery

--- jquery.js
+++ jquery.js
@@ -1,11 +1,29 @@
-var jquery = function(arg){
+// var jquery = function(arg){
+//   if(arg[0] === "."){
+//     return document.getElementsByClassName(arg.slice(1));
+//   }
+//   else if(arg[0] === "#"){
+//     return document.getElementById(arg.slice(1));
+//   }
+//   else{
+//     return document.getElementsByTagName(arg);
+//   };
+// }
+
+function jquery(arg) {
+
+  if (!(this instanceof jquery)) {
+    return new jquery(arg);
+  } 
+
   if(arg[0] === "."){
-    return document.getElementsByClassName(arg.slice(1));
+    return this.collection = document.getElementsByClassName(arg.slice(1));
   }
   else if(arg[0] === "#"){
-    return document.getElementById(arg.slice(1));
+    return this.collection = document.getElementById(arg.slice(1));
   }
   else{
-    return document.getElementsByTagName(arg);
+    return this.collection = document.getElementsByTagName(arg);
   };
-}
+
+};
