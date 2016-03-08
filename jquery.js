c989b0c31553af68672159edb9e2fad746b1f17e
Thomas Lo <thomasjinlo@gmail.com>
2016-03-08 12:01:13 -0800

    Working on .hasClass function

--- jquery.js
+++ jquery.js
@@ -16,19 +16,42 @@ function jquery(arg) {
     return new jquery(arg);
   }
 
-  if(arg[0] === "."){
+  if ( typeof arg === "object" ) {
+    this.collection = arg;
+  }
+  else if(arg[0] === "."){
     this.collection = document.getElementsByClassName(arg.slice(1));
   }
   else if(arg[0] === "#"){
-    this.collection = document.getElementById(arg.slice(1));
+    this.collection = [document.getElementById(arg.slice(1))];
   }
   else{
     this.collection = document.getElementsByTagName(arg);
   };
+
+  this.each = function(func){
+    for ( var i = 0; i < this.collection.length; i++) {
+     func(this.collection[i], i);
+    }
+  };
+
+  this.hasClass = function(arg){
+    this.each(function(element){
+      if ( element.className === arg ) {
+        return true;
+      }
+    })
+  };
+
+
 
   this.length = this.collection.length;
   this.idx = function(idx){
     return this.collection[idx];
   }
   return this
+};
+
+var $ = function(arg){
+  return new jquery(arg);
 };
