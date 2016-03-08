2716f5b5ecd7dcedad3e645618c10e7f3143bb6c
Jeffrey Gisin <jgisin@gmail.com>
2016-03-08 10:34:17 -0700

    first warmups

---
+++ constructors.js
@@ -0,0 +1,24 @@
+// Create a normal constructor called FooConstructor which returns a simple FooConstructor instance containing a sample property and method when you call it with the new keyword.
+// Create your own "constructor" called BarConstructor which returns a simple anonymous object which is otherwise the same as the one you created using FooConstructor above but doesn't need to be called using new.
+
+function FooConstructor(){
+  this.prop = "LOL";
+  this.meth = function(){console.log("hi")};
+}
+
+function BarConstructor() {
+    this.foo = "bar";
+    this.bar_method = function(){"BAR!"};
+    if(!(this instanceof BarConstructor)){
+      return new BarConstructor()
+    }
+}
+
+var foo = new FooConstructor();
+var bar = BarConstructor()
+var newbar = new BarConstructor();
+
+
+console.log(foo);
+console.log(bar);
+console.log(newbar);
