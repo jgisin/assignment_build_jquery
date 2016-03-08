f6ac02634ec1a61d58e2bb1502abbf3b43e2570e
Jeffrey Gisin <jgisin@gmail.com>
2016-03-08 12:35:56 -0700

    jqeurying

--- jquery.js
+++ jquery.js
@@ -14,16 +14,21 @@ function jquery(arg) {
 
   if (!(this instanceof jquery)) {
     return new jquery(arg);
-  } 
+  }
 
   if(arg[0] === "."){
-    return this.collection = document.getElementsByClassName(arg.slice(1));
+    this.collection = document.getElementsByClassName(arg.slice(1));
   }
   else if(arg[0] === "#"){
-    return this.collection = document.getElementById(arg.slice(1));
+    this.collection = document.getElementById(arg.slice(1));
   }
   else{
-    return this.collection = document.getElementsByTagName(arg);
+    this.collection = document.getElementsByTagName(arg);
   };
 
+  this.length = this.collection.length;
+  this.idx = function(idx){
+    return this.collection[idx];
+  }
+  return this
 };
