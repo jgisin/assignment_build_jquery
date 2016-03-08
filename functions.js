971d22bdded7064d1aaf462d20e1c1264c533bfd
Thomas Lo <thomasjinlo@gmail.com>
2016-03-08 09:59:23 -0800

    finished functions!

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
 function SimpleObject() {
-  collection: 
+  this.collection = [];
+  this.each = function(func){
+    for ( var i = 0; i < this.collection.length; i++) {
+     func(this.collection[i], i);
+    }
+  }
 }
+
+var myObj = new SimpleObject();
+myObj.collection = [1, "foo", 3];
+myObj.each( function( el, index ){
+    console.log( "Item " + index + " is " + el)
+})
