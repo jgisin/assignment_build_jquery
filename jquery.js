6bf2d21a3d3de9ce186dd3b798aa2a575a0417d6
Thomas Lo <thomasjinlo@gmail.com>
2016-03-08 15:53:46 -0800

    Fix the bug!

--- jquery.js
+++ jquery.js
@@ -12,12 +12,14 @@
 
 function jquery(arg) {
 
-  if (!(this instanceof jquery)) {
-    return new jquery(arg);
-  }
+  // if (!(this instanceof jquery)) {
+  //   return new jquery(arg);
+  // }
+
+  
 
   if ( typeof arg === "object" ) {
-    this.collection = arg;
+    this.collection = [arg];
   }
   else if(arg[0] === "."){
     this.collection = document.getElementsByClassName(arg.slice(1));
@@ -27,29 +29,62 @@ function jquery(arg) {
   }
   else{
     this.collection = document.getElementsByTagName(arg);
-  };
+  }
 
-  this.each = function(func){
+  this.each = function(funk){
     for ( var i = 0; i < this.collection.length; i++) {
-     func(this.collection[i], i);
+     funk(this.collection[i], i);
     }
   };
 
   this.hasClass = function(arg){
+    var bool = false
+
     this.each(function(element){
       if ( element.className === arg ) {
-        return true;
+        bool = true;
       }
-    })
+    });
+
+    return bool
   };
 
+  this.addClass = function(arg){
+    this.each(function(element){
+      element.className += " " + arg;
+    });
+  };
 
+  this.removeClass = function(arg){
+
+    var arg_names = arg.split(" ");
+
+    this.each(function(element){
+      var class_name = element.className.split(" ");
+
+      for ( var i = 0; i < arg_names.length; i++ ){
+
+        var classNameIdx = class_name.indexOf(arg_names[i]);
+
+        if ( classNameIdx > -1 ) {
+          class_name.splice(classNameIdx, 1);
+        }
+
+      }
+
+      element.className = class_name.join(" ");
+
+    });
+  };
 
   this.length = this.collection.length;
+
   this.idx = function(idx){
     return this.collection[idx];
   }
-  return this
+
+  return this;
+
 };
 
 var $ = function(arg){
